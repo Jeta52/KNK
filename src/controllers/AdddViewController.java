3142af940c2f516d6e1fd9e137474a8ba959c94c
kaltrinabaliu <93614313+kaltrinabaliu@users.noreply.github.com>
2022-05-25 19:22:40 +0200

    Update AdddViewController.java

--- src/controllers/AdddViewController.java
+++ src/controllers/AdddViewController.java
@@ -1,6 +1,9 @@
 package controllers;
 
+import java.io.IOException;
+
 import java.net.URL;
+import java.security.NoSuchAlgorithmException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -8,24 +11,32 @@ import java.sql.SQLException;
 import java.util.ResourceBundle;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.collections.transformation.FilteredList;
 import javafx.collections.transformation.SortedList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Hyperlink;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
 import javax.swing.JOptionPane;
 
 import database.DBConnect;
 import model.AddFlight;
-
 
 
 public class AdddViewController implements Initializable {
