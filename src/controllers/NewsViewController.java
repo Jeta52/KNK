544d78b472c3acfab674bcdb14abe0a871fe715d
Jeta-Kajtazi <94572246+Jeta-Kajtazi@users.noreply.github.com>
2023-10-07 15:33:40 +0200

    "BarChart not fetching data" issue fixed | Code re-format

--- src/controllers/NewsViewController.java
+++ src/controllers/NewsViewController.java
@@ -1,6 +1,5 @@
 package controllers;
 
-
 import java.io.IOException;
 import java.net.URL;
 import java.sql.Connection;
@@ -27,203 +26,185 @@ import javafx.scene.control.MenuItem;
 import javafx.stage.Stage;
 import javafx.scene.input.KeyCodeCombination;
 
+public class NewsViewController implements Initializable {
+  @FXML
+  private MenuButton language;
 
+  @FXML
+  private Button login;
 
+  @FXML
+  private Button news;
 
-public class NewsViewController implements Initializable{
- @FXML
-    private MenuButton language;
+  @FXML
+  private Button signup;
 
-    @FXML
-    private Button login;
+  @FXML
+  private Button arrivals;
 
-    @FXML
-    private Button news;
+  @FXML
+  private Button departures;
 
-    @FXML
-    private Button signup;
+  @FXML
+  private MenuItem eng;
 
-    @FXML
-    private Button arrivals;
+  @FXML
+  private MenuItem ger;
 
+  @FXML
+  private MenuItem al;
 
-    @FXML
-    private Button departures;
-    
-    @FXML
-    private MenuItem eng;
+  @FXML
+  void language(ActionEvent event) throws IOException {
 
-    @FXML
-    private MenuItem ger;
-    
-    @FXML
-    private MenuItem al;
+    eng.setOnAction(a -> {
+      english(a);
+    });
+    ger.setOnAction(a -> {
+      german(a);
+    });
+    al.setOnAction(a -> {
+      albanian(a);
+    });
+    language.getItems().addAll(eng, ger, al);
 
+  }
 
-    @FXML
-    void language(ActionEvent event) throws IOException {
-    	
-    	eng.setOnAction(a-> {
-    		english(a);
-    	});
-    	ger.setOnAction(a-> {
-    		german(a);
-    	});
-    	al.setOnAction(a-> {
-    		albanian(a);
-    	});
-    	language.getItems().addAll(eng,ger,al);
-    	
+  @FXML
+  void login(ActionEvent event) throws IOException {
+    this.login((Node) event.getSource());
+  }
+  private void login(Node source) throws IOException {
+    FXMLLoader loader = new FXMLLoader(
+      getClass().getResource("/views/LoginView.fxml")
+    );
+    Parent pane = loader.load();
+    Scene scene = new Scene(pane, 640, 400);
+    Stage primaryStage = (Stage) source.getScene().getWindow();
+    primaryStage.setScene(scene);
+  }
+
+  @FXML
+  void news(ActionEvent event) throws IOException {
+    this.news((Node) event.getSource());
+  }
+  private void news(Node source) throws IOException {
+    FXMLLoader loader = new FXMLLoader(
+      getClass().getResource("/views/NewsView.fxml")
+    );
+    Parent pane = loader.load();
+    Scene scene = new Scene(pane, 920, 760);
+    Stage primaryStage = (Stage) source.getScene().getWindow();
+    primaryStage.setScene(scene);
+  }
+
+  @FXML
+  void signup(ActionEvent event) throws IOException {
+
+    this.signup((Node) event.getSource());
+
+  }
+
+  private void signup(Node source) throws IOException {
+    FXMLLoader loader = new FXMLLoader(
+      getClass().getResource("/views/SignUpView.fxml")
+    );
+    Parent pane = loader.load();
+    Scene scene = new Scene(pane, 640, 400);
+    Stage primaryStage = (Stage) source.getScene().getWindow();
+    primaryStage.setScene(scene);
+    primaryStage.setResizable(false);
+  }
+
+  @FXML
+  void arrivals(ActionEvent event) throws IOException {
+
+    this.arrivals((Node) event.getSource());
+
+  }
+
+  private void arrivals(Node source) throws IOException {
+    FXMLLoader loader = new FXMLLoader(
+      getClass().getResource("/views/FlightView.fxml")
+    );
+    Parent pane = loader.load();
+    Scene scene = new Scene(pane, 920, 760);
+    Stage primaryStage = (Stage) source.getScene().getWindow();
+    primaryStage.setScene(scene);
+    primaryStage.setResizable(false);
+  }
+
+  @FXML
+  void departures(ActionEvent event) throws IOException {
+
+    this.departures((Node) event.getSource());
+
+  }
+
+  private void departures(Node source) throws IOException {
+    FXMLLoader loader = new FXMLLoader(
+      getClass().getResource("/views/FlightView2.fxml")
+    );
+    Parent pane = loader.load();
+    Scene scene = new Scene(pane, 920, 760);
+    Stage primaryStage = (Stage) source.getScene().getWindow();
+    primaryStage.setScene(scene);
+    primaryStage.setResizable(false);
+
+  }
+  @FXML
+  void english(ActionEvent event) {
+    loadMessages("en");
+
+  }
+
+  @FXML
+  void german(ActionEvent event) {
+    loadMessages("de");
+
+  }
+
+  @FXML
+  void albanian(ActionEvent event) {
+    loadMessages("al");
+
+  }
+
+  private ResourceBundle bundle;
+  private void loadMessages(String messages) {
+    Locale locale = new Locale(messages);
+    bundle = ResourceBundle.getBundle("resources.messages", locale);
+    news.setText(bundle.getString("Button"));
+    login.setText(bundle.getString("Button1"));
+    signup.setText(bundle.getString("Button2"));
+    arrivals.setText(bundle.getString("Button3"));
+    departures.setText(bundle.getString("Button4"));
+    language.setText(bundle.getString("MenuButton"));
+  }
+
+  @FXML
+  public BarChart barchart;
+  DBConnect dbc = DBConnect.getInstance();
+  String tableName = "flights1";
+  String groupby_column = "date1";
+  public XYChart.Series getDataSeriesBarChart() throws SQLException, ClassNotFoundException {
+    ResultSet db_data = dbc.count_values(tableName, groupby_column);
+
+    XYChart.Series dataSeries1 = new XYChart.Series();
+    while (db_data.next()) {
+      int value = db_data.getInt("count(*)");
+      String key = db_data.getString("month_name");
+      dataSeries1.getData().add(new XYChart.Data(key, value));
     }
- 
+    return dataSeries1;
+  }
 
-	@FXML
-    void login(ActionEvent event) throws IOException{
-    	this.login((Node) event.getSource());
-    }
-    private void login(Node source) throws IOException {
-			FXMLLoader loader = new FXMLLoader(
-					getClass().getResource("/views/LoginView.fxml")
-					);
-			Parent pane = loader.load();
-			Scene scene = new Scene(pane, 640, 400);
-			Stage primaryStage = (Stage) source.getScene().getWindow();
-			primaryStage.setScene(scene);
-		}
+  @Override
+  public void initialize(URL arg0, ResourceBundle arg1) {
+    try {
+      this.barchart.getData().add(getDataSeriesBarChart());
+    } catch (Exception ignore) {};
 
-    @FXML
-    void news(ActionEvent event) throws IOException{
-    	this.news((Node) event.getSource());
-    }
-    private void news(Node source) throws IOException {
-		FXMLLoader loader = new FXMLLoader(
-				getClass().getResource("/views/NewsView.fxml")
-				);
-		Parent pane = loader.load();
-		Scene scene = new Scene(pane, 920, 760);
-		Stage primaryStage = (Stage) source.getScene().getWindow();
-		primaryStage.setScene(scene);
-	}
-    
-    @FXML
-    void signup(ActionEvent event )throws IOException {
-    	
-    	this.signup((Node) event.getSource());
-    
-    }
-   
-    private void signup(Node source) throws IOException {
-		FXMLLoader loader = new FXMLLoader(
-				getClass().getResource("/views/SignUpView.fxml")
-				);
-		Parent pane = loader.load();
-		Scene scene = new Scene(pane, 640, 400);
-		Stage primaryStage = (Stage) source.getScene().getWindow();
-		primaryStage.setScene(scene);
-		primaryStage.setResizable(false);		
-	}
+  }
 
-
-    
-    @FXML
-    void arrivals(ActionEvent event )throws IOException {
-    	
-    	this.arrivals((Node) event.getSource());
-    
-    }
-   
-    private void arrivals(Node source) throws IOException {
-		FXMLLoader loader = new FXMLLoader(
-				getClass().getResource("/views/FlightView.fxml")
-				);
-		Parent pane = loader.load();
-		Scene scene = new Scene(pane, 920, 760);
-		Stage primaryStage = (Stage) source.getScene().getWindow();
-		primaryStage.setScene(scene);
-		primaryStage.setResizable(false);	
-    }
-		
-		
-		  
-	    @FXML
-	    void departures(ActionEvent event )throws IOException {
-	    	
-	    	this.departures((Node) event.getSource());
-	    
-	    }
-	   
-	    private void departures(Node source) throws IOException {
-			FXMLLoader loader = new FXMLLoader(
-					getClass().getResource("/views/FlightView2.fxml")
-					);
-			Parent pane = loader.load();
-			Scene scene = new Scene(pane, 920, 760);
-			Stage primaryStage = (Stage) source.getScene().getWindow();
-			primaryStage.setScene(scene);
-			primaryStage.setResizable(false);	
-
-
-	    }
-	    @FXML
-	   void english(ActionEvent event) {
-	    	loadMessages("en");
-	    	
-	    }
-
-	    @FXML
-	    void german(ActionEvent event) {
-	         loadMessages("de");
-	    
-	    }
-	    
-	    @FXML
-	    void albanian(ActionEvent event) {
-	          loadMessages("al");
-	    	
-	    }
-	    
-private ResourceBundle bundle;
-private void loadMessages(String messages) {
-	Locale locale = new Locale(messages);
-    bundle = ResourceBundle.getBundle("resources.messages",locale);
-	news.setText(bundle.getString("Button"));
-	login.setText(bundle.getString("Button1"));
-	signup.setText(bundle.getString("Button2"));
-	arrivals.setText(bundle.getString("Button3"));
-	departures.setText(bundle.getString("Button4"));
-	language.setText(bundle.getString("MenuButton"));
-}
-
-		
-	    @FXML
-	    public BarChart barchart;
-	    DBConnect dbc = DBConnect.getInstance();
-	    String tableName = "flights1";
-	    String groupby_column = "date1";
-	    public XYChart.Series getDataSeriesBarChart() throws SQLException, ClassNotFoundException {
-	        ResultSet db_data = dbc.count_values(tableName, groupby_column);
-
-	        XYChart.Series dataSeries1 = new XYChart.Series();
-	        while (db_data.next()) {
-	        	
-	            int value = db_data.getInt("count(*)");
-	            String cond = "monthname("+groupby_column+")";
-	            String key = db_data.getString(cond);
-	            dataSeries1.getData().add(new XYChart.Data(key, value));
-	        }
-	        return dataSeries1;
-	    }
-	    
-
-	 
-		@Override
-		public void initialize(URL arg0, ResourceBundle arg1) {
-		      try {
-		            this.barchart.getData().add(getDataSeriesBarChart());
-		        }
-		        catch (Exception ignore) {};
-			
-		}
-	 
-	    
 }
